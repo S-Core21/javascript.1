9f2c12a7ac78ef019650d20d701a18acb3ccd0d1
Chukwuebuka Ogbonna <ogbonnashedrach8@gmail.com>
2022-06-22 20:54:20 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function personalDetails(){
+    let name = "Fullname: Ogbonna Chukwuebuka Shedrach      " 
+    let height = 'Height: 180cm         '
+    let country = 'Country: Nigeria'  
+ console.log(name + height + country)
+ alert(name + height + country)
+}
+personalDetails();
